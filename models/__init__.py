2e7605c177e32a1fa5fa2773af5e5978c33671c2
AdhamKhaledDev <adhamk109@outlook.com>
2024-03-11 16:30:55 -0700

    Delete and refresh

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/bin/usr/python3
-
-from .base_model import BaseModel
-from .engine import FileStorage
-
-storage = FileStorage()  # Create a FileStorage instance
-storage.reload()  # Reload objects from file if it exists
